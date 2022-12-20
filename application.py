eeb20102443e6712d400e7f4042cd872c95aa2ba
ZYbunny612 <37568443+ZYbunny612@users.noreply.github.com>
2022-12-21 07:26:45 +0800

    Update application.py

--- application.py
+++ application.py
@@ -299,7 +299,7 @@ def get_customer_by_email(email):
 
 if __name__=='__main__':
 
-    application.run(host='0.0.0.0', port=8080, debug=True)
+    application.run(host='0.0.0.0', port=8000, debug=True)
 
 
 
